7eb6cb22720b61c58dff252ee5cb42c7b4bedbec
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-14 20:48:39 -0300

    cria interface especifica para Tarefa

---
+++ TarefasApp.Domain/Interfaces/Repositories/ITarefaRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasApp.Domain.Entities;
+
+namespace TarefasApp.Domain.Interfaces.Repositories
+{   
+    /// <summary>
+    /// interface de repositorio especifica para tarefa
+    /// </summary>
+    public interface ITarefaRepository: IBaseRepository<Tarefa>
+    {
+    }
+}
