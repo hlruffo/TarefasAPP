ba5f40a43b7393e5e0675e1718bef7cb49443e51
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-14 20:36:12 -0300

    cria a entidade tarefa em domain

---
+++ TarefasApp.Domain/Entities/Tarefa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefasApp.Domain.Entities
+{
+    public class Tarefa
+    {
+        public Guid? Id { get; set; }
+        public string? Nome { get; set; }
+        public DateTime? DataHora { get; set; }
+        public string? Descricao { get; set; }
+        public PrioridadeTarefa? Prioridade { get; set;}
+    }
+    /// <summary>
+    /// Enum para definir prioridade
+    /// </summary>
+    public enum PrioridadeTarefa
+    {
+        Baixa = 1,
+        Media = 2,
+        Alta = 3
+    }
+}
