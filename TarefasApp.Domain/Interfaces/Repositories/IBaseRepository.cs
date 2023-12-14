7e774fc2675771b0fa5e76deca8ab5c1ec39e8d9
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-14 20:47:02 -0300

    cria a interface base para repositorio

---
+++ TarefasApp.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefasApp.Domain.Interfaces.Repositories
+{
+    /// <summary>
+    /// interface generica para construção dos métodos do repositório
+    /// </summary>
+    public interface IBaseRepository<T>
+        where T : class
+
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetAll();
+        T GetById(Guid id);
+    }
+}
