edf354617daa0f6bc540be674ffbae005b9addb2
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-14 18:46:46 -0300

    cria as tarefas

---
+++ TarefasApp.Services/Controllers/TarefasController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TarefasApp.Services.Controllers
+{
+    /// <summary>
+    /// Classe de controle para o ENDPOINT de serviço de tarefas da API
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        /// <summary>
+        /// Método para cadastrar tarefas.
+        /// </summary>
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Cadastro de tarefa!");
+        }
+
+        /// <summary>
+        /// Método para atualizar tarefas.
+        /// </summary>
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok("Edição de tarefa!");
+        }
+
+        /// <summary>
+        /// Método para exclusão de tarefas.
+        /// </summary>
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok("Exclusão de tarefa!");
+        }
+    }
+}
+
+
