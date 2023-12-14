ab0c841c8d1009711bb460d0abd55e1df9626d58
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-14 19:27:40 -0300

    cria Models para POST

---
+++ TarefasApp.Services/Models/TarefasPostModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TarefasApp.Services.Models
+{
+    public class TarefasPostModel
+    {
+        [Required(ErrorMessage = "Por favor, informe o nome da tarefa.")]
+        [MinLength(8, ErrorMessage = "Por favor, informe o mínimo de {1} caractéres.")]
+        [MaxLength(100, ErrorMessage = "Por favor, informe o máximo de {1} caractéres.")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe a data e hora da tarefa.")]
+        public DateTime? DataHora { get; set;}
+
+        [Required(ErrorMessage = "Por favor, informe a descrição  da tarefa.")]
+        [MinLength(8, ErrorMessage = "Por favor, informe o mínimo de {1} caractéres.")]
+        [MaxLength(250, ErrorMessage = "Por favor, informe o máximo de {1} caractéres.")]
+        public string? Descricao { get; set; }
+
+
+        [Required(ErrorMessage = "Por favor, informe a prioridade da tarefa.")]
+        [Range(1,3, ErrorMessage ="Por favor, informe valor 1(Baixa), 2(Média) ou 3(Alta) para prioridade")]
+        public int? Prioridade { get; set; }    
+    }
+}
