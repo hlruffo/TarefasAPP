a0d9bbc0ba9bb78fd5b9949d316001609a17d69d
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-14 21:17:32 -0300

    implemetnta TarefaRepository

---
+++ TarefasApp.Infra.Data/Repositories/TarefaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasApp.Domain.Entities;
+using TarefasApp.Domain.Interfaces.Repositories;
+
+namespace TarefasApp.Infra.Data.Repositories
+{
+    /// <summary>
+    /// Repositorio do banco de dados para tarefa
+    /// </summary>
+    public class TarefaRepository : BaseRepository<Tarefa>, ITarefaRepository
+    {
+
+    }
+}
