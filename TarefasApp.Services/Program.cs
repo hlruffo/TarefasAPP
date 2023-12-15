871b5754adc0c7235542b4d14151dafdc2e845ed
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-14 21:47:47 -0300

    Registra DependencyInjection na program de services

--- TarefasApp.Services/Program.cs
+++ TarefasApp.Services/Program.cs
@@ -1,3 +1,5 @@
+using TarefasApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//configuração de injeção de dependencia
+DependencyInjection.Register(builder.Services);
 
 var app = builder.Build();
 
