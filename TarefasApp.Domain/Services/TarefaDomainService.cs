b872038fe47baa5321d50fa0ef48de3b2052a9b8
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-14 21:42:27 -0300

    implementa a service TarefaDomainService e Dependency Injection

---
+++ TarefasApp.Domain/Services/TarefaDomainService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasApp.Domain.Entities;
+using TarefasApp.Domain.Interfaces.Repositories;
+using TarefasApp.Domain.Interfaces.Services;
+
+namespace TarefasApp.Domain.Services
+{
+    /// <summary>
+    /// Classe de serviços de dominio para tarefa
+    /// </summary>
+    public class TarefaDomainService : ITarefaDomainService
+    {
+        private readonly ITarefaRepository? _tarefaRepository;
+
+        public TarefaDomainService(ITarefaRepository? tarefaRepository)
+        {
+            _tarefaRepository = tarefaRepository;
+        }
+
+        public void Cadastrar(Tarefa tarefa)
+        {
+          _tarefaRepository?.Add(tarefa);
+        }
+    }
+}
---
+++ TarefasApp.Services/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using TarefasApp.Domain.Interfaces.Services;
+using TarefasApp.Domain.Interfaces.Repositories;
+using TarefasApp.Domain.Services;
+using TarefasApp.Infra.Data.Repositories;
+
+namespace TarefasApp.Services
+{
+    /// <summary>
+    /// Classe para configurar as injeções de dependência do projeto
+    /// </summary>
+    public class DependencyInjection
+    {
+        /// <summary>
+        /// Método para configurar as injeções de dependência do sistema
+        /// </summary>
+        public static void Register(IServiceCollection services)
+        {
+            services.AddTransient < ITarefaDomainService, TarefaDomainService >();
+            services.AddTransient<ITarefaRepository, TarefaRepository>();
+        }
+    }
+}
+
+
+
