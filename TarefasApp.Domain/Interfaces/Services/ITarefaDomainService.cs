48f23fcc208365eb5e262d4e63ab8b4143d99c64
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-14 21:23:58 -0300

    cria a interface para métodos de serviço do dominio referente a tarefa

---
+++ TarefasApp.Domain/Interfaces/Services/ITarefaDomainService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasApp.Domain.Entities;
+
+namespace TarefasApp.Domain.Interfaces.Services
+{
+    /// <summary>
+    /// interface para os métodos de serviço de dominio de tarefa
+    /// </summary>
+    public interface ITarefaDomainService
+    {
+        void Cadastrar(Tarefa tarefa);
+
+    }
+}
