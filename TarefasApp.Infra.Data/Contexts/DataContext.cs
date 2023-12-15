731c25daac2ea15429032f23884cf8bc01cefb59
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-14 21:05:04 -0300

    cria o DataContext

---
+++ TarefasApp.Infra.Data/Contexts/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasApp.Domain.Entities;
+
+namespace TarefasApp.Infra.Data.Contexts
+{
+    /// <summary>
+    /// classe para mapeamento da conexão do banco de dados
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// metódo para configurar o BD do projeto
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName:"BDTarefasApp" );
+        }
+
+        //DbSet -> fornece os métodos CRUD para cada entidade
+        public DbSet<Tarefa>? Tarefa { get; set; }
+    }
+}
