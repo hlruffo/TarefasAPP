f07b53fc8dead561a4085201e7ae4a9ea744cbe8
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-19 20:44:35 -0300

    Cria o serviço para implementação da interface de serviço da categoria

---
+++ TarefasApp.Domain/Services/CategoriaDomainService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasApp.Domain.Entities;
+using TarefasApp.Domain.Interfaces.Services;
+
+namespace TarefasApp.Domain.Services
+{
+    public class CategoriaDomainService : ICategoriaDomainService
+    {
+        public List<Categoria> Consultar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
