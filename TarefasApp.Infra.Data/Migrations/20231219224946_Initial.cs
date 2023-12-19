afdb97a977bbfe9987ccf58990d158cd0f866e67
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-19 19:51:53 -0300

    cria e aplica as migrations do BD

---
+++ TarefasApp.Infra.Data/Migrations/20231219224946_Initial.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TarefasApp.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CATEGORIA",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NOME = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CATEGORIA", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TAREFA",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NOME = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DATAHORA = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DESCRICAO = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    PRIORIDADE = table.Column<int>(type: "int", nullable: false),
+                    CATEGORIAID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TAREFA", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_TAREFA_CATEGORIA_CATEGORIAID",
+                        column: x => x.CATEGORIAID,
+                        principalTable: "CATEGORIA",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CATEGORIA_NOME",
+                table: "CATEGORIA",
+                column: "NOME",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TAREFA_CATEGORIAID",
+                table: "TAREFA",
+                column: "CATEGORIAID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TAREFA");
+
+            migrationBuilder.DropTable(
+                name: "CATEGORIA");
+        }
+    }
+}
