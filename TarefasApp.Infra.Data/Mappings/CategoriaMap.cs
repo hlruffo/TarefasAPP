35567c8e8d4c5b3fb15cbbb28dad82196ce06d8c
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-19 19:19:02 -0300

    mapeia Categoria

---
+++ TarefasApp.Infra.Data/Mappings/CategoriaMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasApp.Domain.Entities;
+
+namespace TarefasApp.Infra.Data.Mappings
+{
+    public class CategoriaMap : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.ToTable("CATEGORIA");
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).HasColumnName("ID");
+            builder.Property(c => c.Nome).HasColumnName("NOME").HasMaxLength(50).IsRequired();
+            builder.HasIndex(c=>c.Nome).IsUnique();
+        }
+    }
+}
