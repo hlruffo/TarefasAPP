4d51cd57026e815b868f826001ad6c51f7259001
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-19 20:46:39 -0300

    cria em CategoriaDomainService a injeção de dependencia do repositorio ICategoriaRepository

--- TarefasApp.Domain/Services/CategoriaDomainService.cs
+++ TarefasApp.Domain/Services/CategoriaDomainService.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TarefasApp.Domain.Entities;
+using TarefasApp.Domain.Interfaces.Repositories;
 using TarefasApp.Domain.Interfaces.Services;
 
 namespace TarefasApp.Domain.Services
 {
     public class CategoriaDomainService : ICategoriaDomainService
     {
+        private readonly ICategoriaRepository? _categoriarepository;
+
+        public CategoriaDomainService(ICategoriaRepository? categoriarepository)
+        {
+            _categoriarepository = categoriarepository;
+        }
+
         public List<Categoria> Consultar()
         {
             throw new NotImplementedException();
