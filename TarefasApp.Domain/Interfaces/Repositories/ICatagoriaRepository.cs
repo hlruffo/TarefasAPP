7645ef98ab016e071bfe861147f402d46cd029e2
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-19 20:03:25 -0300

    cria a interface  de repositorio para categoria

---
+++ TarefasApp.Domain/Interfaces/Repositories/ICatagoriaRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasApp.Domain.Entities;
+
+namespace TarefasApp.Domain.Interfaces.Repositories
+{
+    public interface ICatagoriaRepository : IBaseRepository<Categoria>
+    {
+
+    }
+}
