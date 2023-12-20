c7da54c9106dc683c8118d89c0ced27d59cf5ce1
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-19 21:15:17 -0300

    cria controller para categoria -> endpoint get

---
+++ TarefasApp.Services/Controllers/CategoriasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TarefasApp.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
