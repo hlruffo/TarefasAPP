202c77783d27c9e7c5781ce48db9c07c44398fdc
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-21 19:19:50 -0300

    cria mapper entre tarefapostmodel e tarefa

---
+++ TarefasApp.Services/Mappings/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using TarefasApp.Domain.Entities;
+using TarefasApp.Services.Models;
+
+namespace TarefasApp.Services.Mappings
+{
+    public class AutoMapperConfig :Profile
+
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<TarefasPostModel, Tarefa>()
+                .AfterMap((model, entity) =>
+                {
+                    entity.Id = Guid.NewGuid();
+                }
+            );
+        }
+    }
+}
