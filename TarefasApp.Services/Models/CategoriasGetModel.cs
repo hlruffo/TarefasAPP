3a38de3b07030c2bae1017864b6f9d5da56067b9
Hugo L. Ruffo <hlruffo@gmail.com>
2023-12-21 20:13:48 -0300

    cira model CategoriasGetModel

---
+++ TarefasApp.Services/Models/CategoriasGetModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TarefasApp.Services.Models
+{
+    /// <summary>
+    /// Modelo de dados para a resposta de consulta de categorias 
+    /// GET /api/categorias
+    /// </summary>
+    public class CategoriasGetModel
+    {
+        public Guid? Id { get; set; }
+        public string? Nome { get; set;}
+    }
+}
